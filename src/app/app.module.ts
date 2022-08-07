a8a65d9bc87ff5394315cc8ea6aa14f09fa2800d
HectorMartinezCred <hector.martinez.cred@outlook.com>
2022-08-07 15:49:37 -0500

    ProductService
    Create structure of the Service Product.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//external
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { ListProductComponent } from './product/list-product/list-product.component';
 import { DetailProductComponent } from './product/detail-product/detail-product.component';
 import { UpdateProductComponent } from './product/update-product/update-product.component';
 import { AddProductComponent } from './product/add-product/add-product.component';
+
+//external
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -23,6 +27,8 @@ import { AddProductComponent } from './product/add-product/add-product.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
   ],
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  productURL = 'http://localhost:8080/product/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public list(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.productURL + 'list');
+  }
+  public detail(id: number): Observable<Product>{
+    return this.httpClient.get<Product>(this.productURL + `detail/${id}`);
+  }
+  public detailname(name: string): Observable<Product>{
+    return this.httpClient.get<Product>(this.productURL + `detailname/${name}`);
+  }
+  public save(product: Product): Observable<any> {
+    return this.httpClient.post<any>(this.productURL + 'create', product);
+  }
+  public update(id: number, product: Product): Observable<any> {
+    return this.httpClient.put<any>(this.productURL + `update/${id}`, product);
+  }
+  public delete(id: number): Observable<Product>{
+    return this.httpClient.delete<any>(this.productURL + `delete/${id}`);
+  }
 }
